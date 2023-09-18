82afb3928e4b7f49d24490ca613e640cebc7bb0a
Peace50 <oyunwolapeace@gmail.com>
2023-09-18 22:22:48 +0000

    Write a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,25 @@
 #include "main.h"
-/**
- * swap_int - Swaps the values of two integers.
- *
- * Description:
- * This function takes two integer pointers as input and swaps the values
- * of the integers they point to.
- *
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- */
-void swap_int(int *a, int *b)
-{
-	int temp;
 
-	if (a != NULL && b != NULL)
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-		temp = *a;
-		*a = *b;
-		*b = temp;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
 
