270ed6cbc5361febceb67f4d048df0445d741d7d
Peace50 <oyunwolapeace@gmail.com>
2023-10-10 21:50:46 +0000

    Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef MACRO_LIKE_HF
+#define MACRO_LIKE_HF
+/**
+  *File: 4-sum.h
+  *
+  *@x: input value (an int)
+  *@y: input value (an int)
+  *
+  *Description: computes sum of two numbers
+  */
+#define SUM(x, y) (x + y)
+
+#endif
+
