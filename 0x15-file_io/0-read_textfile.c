79fa7b3c759f33757c436cfb83c9f76644fcce31
Peace50 <oyunwolapeace@gmail.com>
2023-10-31 12:17:45 +0000

    Write a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,34 +15,39 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 	ssize_t bytes_read;
-	ssize_t bytes_written;
+	ssize_t bytes_written = 0;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+
+	}
+
+	buffer = (char *)malloc(letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		free(buffer);
+		close(fd);
+
+		if (bytes_written == -1 || bytes_written != bytes_read)
 			return (0);
+	}
 
-			fd = open(filename, O_RDONLY);
-			if (fd == -1)
-			return (0);
-
-			buffer = (char *)malloc(letters);
-			if (buffer == NULL)
-			{
-			close(fd);
-			return (0);
-			}
-			bytes_read = read(fd, buffer, letters);
-
-			if (bytes_read == -1)
-			{
-			free(buffer);
-			close(fd);
-			return (0);
-			bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-			free(buffer);
-			close(fd);
-			if (bytes_written == -1 || bytes_written != bytes_read)
-				return (0);
-
-			return (bytes_written);
-			}
+	return (bytes_written);
 }
